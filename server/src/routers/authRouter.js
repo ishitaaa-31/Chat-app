7cf2e93ed34ee8537fed804997a395cb3c8f0e80
ishitaaa-31 <31ishitabajpai@gmail.com>
2026-03-09 19:27:46 +0530

    feat(auth): add Google login functionality and update dependencies

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { UserRegister, UserLogin, UserLogout } from "../controllers/authController.js";
+import {
+  UserRegister,
+  UserLogin,
+  GoogleUserLogin,
+} from "../controllers/authController.js";
+import { GoogleProtect } from "../middlewares/googleMiddleware.js";
 
 const router = express.Router();
 router.post("/register", UserRegister);
 router.post("/login", UserLogin);
-router.get("/logout", UserLogout);
- export default router;
+router.post("/googleLogin", GoogleProtect, GoogleUserLogin);
+export default router;
